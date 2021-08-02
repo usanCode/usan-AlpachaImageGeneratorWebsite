805c5ea139e4ff2dce0ff76b73593249d7e0705b
usanCode <60264357+usanCode@users.noreply.github.com>
2021-08-02 13:27:24 +0200

    Clicking main buttons for generating needed buttons : problem fixed.Carlos Fortes

--- alpachaJs.js
+++ alpachaJs.js
@@ -1,65 +1,59 @@
-/*const buttonsContainer = document.getElementById('buttons-container');
-const buttonAdder = document.getElementById('buttons-adder');
 
-buttonAdder.addEventListener('click', () => {
-	addButtons(buttonsContainer, 5);
-});
 
-function addButtons(container, quantity) {
-	const buttons = document.createDocumentFragment();
-  
-	for (let i = 0; i < quantity; i++) {
-  	const newButton = document.createElement('button');
-    
-    // here you can set all attributes that you need
-    newButton.setAttribute('id', 'button-' + i);
-    newButton.innerHTML = 'Button #' + i;
-    
-    buttons.appendChild(newButton);
-  }
-  
-  buttonsContainer.appendChild(buttons);
-}*/
- 
-/*
-function myFunction(id) {
-  var x = document.getElementById(id);
-  
-    if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}  
-*/
+function unShow(){
+  var showElements = document.querySelectorAll('.show')
+  showElements.forEach(element =>{element.classList.remove('show')})
+
+}
 
 function myFunction(id){
   var x = document.getElementById(id);
-  switch (x) {
+  
+  switch (id) {
       case id="Backgrounds":
-        document.getElementById(id).style.display = "block";
+        
+        unShow();  // this will unshow all buttons
+        x.classList.add('show');// to eliminate repetition you can use the x instead of document.getElementById(id);
         break;
       case id="Ears":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
       case id="Hair":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
       case id="Eyes":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
       case id="Mouth":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
       case id="Legs":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
       case  id="Neck":
-        document.getElementById(id).style.display = "block";
+        unShow();
+         x.classList.add('show');
         break;
-      case  id="Acessories":
-        document.getElementById(id).style.display = "block";
+      case  id="Accessories":
+        unShow();
+         x.classList.add('show');
         }
         
      }
 
+     function showImg( id ) {
+      // hide previously shown image
+      for ( i = 1; i < 18; i++) {
+          var obj = document.getElementById( "picture" + i );
+          if (obj != null)
+              obj.className = 'hide1';
+      }
+      var obj = document.getElementById( "picture" + id );      
+      if (obj != null)
+          obj.className = 'show1';
+    }
